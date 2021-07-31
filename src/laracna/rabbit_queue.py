49c5036e1c135bd2edbf6ad5f75d4c8fe1b01658
Gavin Hurlbut <gjhurlbu@gmail.com>
2021-07-30 19:34:57 -0700

    Try a persistent connection

--- src/laracna/rabbit_queue.py
+++ src/laracna/rabbit_queue.py
@@ -19,54 +19,52 @@ class RabbitQueue(object):
         self.exchange = exchange_name
         self.key = key
 
-        with pika.BlockingConnection() as connection:
-            channel = connection.channel()
-            channel.exchange_declare(self.exchange, "direct")
-            result = channel.queue_declare(queue='', exclusive=True)
-            self.queue_name = result.method.queue
-            channel.queue_bind(exchange=self.exchange, queue=self.queue_name, routing_key=self.key)
+        self.connection = pika.BlockingConnection()
+        self.channel = self.connection.channel()
+
+        self.channel.exchange_declare(self.exchange, "direct")
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.queue_name = result.method.queue
+        self.channel.queue_bind(exchange=self.exchange, queue=self.queue_name, routing_key=self.key)
+
+    def __del__(self):
+        self.connection.close()
 
     def send(self, message):
-        with pika.BlockingConnection() as connection:
-            channel = connection.channel()
+        if isinstance(message, (list, dict)):
+            message = json.dumps(message)
+        if not isinstance(message, str):
+            raise TypeError("Messages must be string, list or dict")
 
-            if isinstance(message, (list, dict)):
-                message = json.dumps(message)
-            if not isinstance(message, str):
-                raise TypeError("Messages must be string, list or dict")
-
-            channel.basic_publish(exchange=self.exchange, routing_key=self.key,
-                                  body=message.encode("utf-8"))
+        self.channel.basic_publish(exchange=self.exchange, routing_key=self.key,
+                              body=message.encode("utf-8"))
 
     def poll(self):
-        with pika.BlockingConnection() as connection:
-            channel: BlockingChannel = connection.channel()
+        for (method_frame, properties, body) in self.channel.consume(queue=self.queue_name, exclusive=True):
+            try:
+                message = json.loads(body)
+            except Exception:
+                message = body
 
-            for (method_frame, properties, body) in channel.consume(queue=self.queue_name, exclusive=True):
-                try:
-                    message = json.loads(body)
-                except Exception:
-                    message = body
+            item = {
+                "frame": method_frame,
+                "properties": properties,
+                "body": body,
+                "message": message,
+            }
 
-                item = {
-                    "frame": method_frame,
-                    "properties": properties,
-                    "body": body,
-                    "message": message,
-                }
+            # Acknowledge the message
+            self.channel.basic_ack(method_frame.delivery_tag)
 
-                # Acknowledge the message
-                channel.basic_ack(method_frame.delivery_tag)
+            if "shutdown" in message:
+                item = {}
 
-                if "shutdown" in message:
-                    item = {}
+            yield item
 
-                yield item
+            if not item:
+                break
 
-                if not item:
-                    break
-
-            # Cancel the consumer and return any pending messages
-            requeued_messages = channel.cancel()
-            print('Requeued %i messages' % requeued_messages)
+        # Cancel the consumer and return any pending messages
+        requeued_messages = self.channel.cancel()
+        print('Requeued %i messages' % requeued_messages)
 
