9eeee01c12f4f685f651ee4be4193e84bbb0a67d
Gavin Hurlbut <gjhurlbu@gmail.com>
2021-07-30 22:10:19 -0700

    Auto abort after 10s with no new queued items

--- src/laracna/scraper.py
+++ src/laracna/scraper.py
@@ -88,84 +88,96 @@ class Scraper(object):
     def stop_scraping(self):
         self.abort = True
         self.incoming_queue.put({})
+        self.wait()
+
+    def wait(self):
         self.scrape_thread.join()
         self.scrape_thread = None
 
     def scrape_thread_loop(self):
-        while not self.abort:
-            try:
-                item = self.incoming_queue.get()
-            except Exception:
-                continue
-
-            if not item or self.abort:
-                self.abort = True
-                self.queue(item=None)
-                self.outgoing_queue.put({})
-                return
-
-            delay = item.get("delay", None)
-            url = item.get("url", None)
-            method = item.get("method", "GET")
-            body = item.get("body", None)
-            type_ = item.get("type", None)
-            content_type = item.get("content=type", None)
-
-            if isinstance(body, (list, dict)):
-                body = json.dumps(body)
-                content_type = "application/json"
-
-            if delay is None:
-                delay = self.min_delay
-            delay /= 1000.0
-            time.sleep(delay)
-
-            try:
-                if method == "GET":
-                    cache_item = self.cache.get(url)
-                    if cache_item is None:
-                        try:
-                            response = self.session.request(method, url)
-                            self.cache.put(url, response.status_code, response.content)
-                        except Exception as e:
-                            self.cache.put(url, 500, "")
-
-                        cache_item = self.cache.get(url)
-
-                    (code, body) = cache_item
-                else:
-                    if content_type:
-                        headers = {
-                            "Content-Type": content_type,
-                        }
+        try:
+            done = 0
+            while not self.abort:
+                try:
+                    item = self.incoming_queue.get(block=False)
+                    done = 0
+                except Exception:
+                    if done >= 10:
+                        self.abort = True
                     else:
-                        headers = None
+                        done += 1
+                        time.sleep(1.0)
+                    continue
 
-                    response = self.session.request(method, url, data=body, headers=headers)
-                    code = response.status_code
-                    body = response.content.decode("utf-8")
-            except Exception:
-                continue
+                if not item or self.abort:
+                    return
 
-            callback = self.callbacks.get(type_, None)
-            if callback is None:
-                results = body
-                chain_items = []
-            elif hasattr(callback, "__call__"):
-                (results, chain_items) = callback(code, body)
-            else:
-                raise NotImplementedError("No runnable callback for type %s" % type)
+                delay = item.get("delay", None)
+                url = item.get("url", None)
+                method = item.get("method", "GET")
+                body = item.get("body", None)
+                type_ = item.get("type", None)
+                content_type = item.get("content=type", None)
 
-            for chain_item in chain_items:
-                self.queue(item=chain_item)
+                if isinstance(body, (list, dict)):
+                    body = json.dumps(body)
+                    content_type = "application/json"
 
-            out_item = {
-                "url": url,
-                "type": type_,
-                "code": code,
-                "results": results,
-            }
-            self.outgoing_queue.put(out_item)
+                if delay is None:
+                    delay = self.min_delay
+                delay /= 1000.0
+                time.sleep(delay)
+
+                try:
+                    if method == "GET":
+                        cache_item = self.cache.get(url)
+                        if cache_item is None:
+                            try:
+                                response = self.session.request(method, url)
+                                self.cache.put(url, response.status_code, response.content)
+                            except Exception as e:
+                                self.cache.put(url, 500, "")
+
+                            cache_item = self.cache.get(url)
+
+                        (code, body) = cache_item
+                    else:
+                        if content_type:
+                            headers = {
+                                "Content-Type": content_type,
+                            }
+                        else:
+                            headers = None
+
+                        response = self.session.request(method, url, data=body, headers=headers)
+                        code = response.status_code
+                        body = response.content.decode("utf-8")
+                except Exception:
+                    continue
+
+                callback = self.callbacks.get(type_, None)
+                if callback is None:
+                    results = body
+                    chain_items = []
+                elif hasattr(callback, "__call__"):
+                    (results, chain_items) = callback(code, body)
+                else:
+                    raise NotImplementedError("No runnable callback for type %s" % type)
+
+                for chain_item in chain_items:
+                    self.queue(item=chain_item)
+
+                if results:
+                    out_item = {
+                        "url": url,
+                        "type": type_,
+                        "code": code,
+                        "results": results,
+                    }
+                    self.outgoing_queue.put(out_item)
+        finally:
+            self.abort = True
+            self.outgoing_queue.put({})
 
     def get_results(self):
         results = []
